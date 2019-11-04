4f6b7836027f1a5b3908cbb14ad02893ade2e731
Tristan Pendergrass <tristanp@dropbox.com>
2019-11-04 10:30:02 -0800

    Add ability to build into chrome extension

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,55 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (h1, text)
+import Html exposing (Html, h1, text)
 
 
+main : Program () Model Msg
 main =
-    h1 [] [ text "Hello, Elm!" ]
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( {}, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view _ =
+    h1 [] [ text "Hello SOM..." ]
