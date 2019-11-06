9a3b0c033c573e0b0d6a09a6515dcbd4ac280376
Tristan Pendergrass <tristanp@dropbox.com>
2019-11-06 10:55:54 -0800

    Add list of overrides and ability to choose one and apply it

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,14 @@
 port module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, h1, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, h1, li, option, select, span, text, ul)
+import Html.Attributes exposing (value)
+import Html.Events exposing (onClick, onInput)
 import Json.Encode as E
+import List
+import Set exposing (Set)
+import String
+import Task
 
 
 main : Program String Model Msg
@@ -15,14 +20,42 @@ main =
 -- MODEL
 
 
+type alias Id =
+    Int
+
+
+type VariantSelection
+    = NotSelected
+    | Variant String
+
+
+type alias Override =
+    { id : Id
+    , feature : String
+    , variants : List String
+    , selectedVariant : VariantSelection
+    }
+
+
 type alias Model =
-    { browserUrl : String
+    { nonce : Int
+    , browserUrl : String
+    , overrides : List Override
     }
 
 
 init : String -> ( Model, Cmd Msg )
 init initialBrowserUrl =
-    ( { browserUrl = initialBrowserUrl }
+    ( { nonce = 100
+      , browserUrl = initialBrowserUrl
+      , overrides =
+            [ { id = 0
+              , feature = "foo"
+              , variants = [ "OFF", "ON" ]
+              , selectedVariant = NotSelected
+              }
+            ]
+      }
     , Cmd.none
     )
 
@@ -37,6 +70,20 @@ port sendUrl : String -> Cmd msg
 type Msg
     = SetBrowserUrl String
     | SendBrowserUrl String
+    | HandleSelectedVariantInput Override String
+    | ApplyOverrides
+
+
+replace : a -> a -> List a -> List a
+replace oldA newA =
+    List.map
+        (\ax ->
+            if ax == oldA then
+                newA
+
+            else
+                ax
+        )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -51,7 +98,47 @@ update msg model =
                 newUrl =
                     model.browserUrl ++ url
             in
-            ( { model | browserUrl = newUrl }, sendUrl <| newUrl )
+            ( { model | browserUrl = newUrl }, sendUrl newUrl )
+
+        HandleSelectedVariantInput override selection ->
+            let
+                newOverride : Override
+                newOverride =
+                    { override
+                        | selectedVariant =
+                            if selection == "" then
+                                NotSelected
+
+                            else
+                                Variant selection
+                    }
+
+                newOverrides : List Override
+                newOverrides =
+                    replace override newOverride model.overrides
+            in
+            ( { model | overrides = newOverrides }, Cmd.none )
+
+        ApplyOverrides ->
+            let
+                reduceOverrides : Override -> List String -> List String
+                reduceOverrides override accumulator =
+                    case override.selectedVariant of
+                        NotSelected ->
+                            accumulator
+
+                        Variant value ->
+                            ("stormcrow_override=" ++ override.feature ++ ":" ++ value) :: accumulator
+
+                parameters : List String
+                parameters =
+                    List.foldl reduceOverrides [] model.overrides
+
+                newUrl : String
+                newUrl =
+                    model.browserUrl ++ "?" ++ String.join "&" parameters
+            in
+            ( model, sendUrl newUrl )
 
 
 
@@ -70,9 +157,34 @@ subscriptions _ =
 -- VIEW
 
 
+renderOverride : Override -> Html Msg
+renderOverride override =
+    let
+        renderOption : String -> Html Msg
+        renderOption variant =
+            option [ value variant ] [ text variant ]
+
+        selectValue : String
+        selectValue =
+            case override.selectedVariant of
+                NotSelected ->
+                    ""
+
+                Variant value ->
+                    value
+    in
+    li []
+        [ span [] [ text override.feature ]
+        , select [ onInput (HandleSelectedVariantInput override), value selectValue ] (List.map renderOption ("" :: override.variants))
+        , span [] [ text ("Selected: " ++ selectValue) ]
+        ]
+
+
 view : Model -> Html Msg
 view model =
     div []
         [ h1 [] [ text ("Url: " ++ model.browserUrl) ]
         , button [ onClick (SendBrowserUrl "&stormcrow_override=browse_rename_use_api_v2:ON") ] [ text "Set URL" ]
+        , ul [] (List.map renderOverride model.overrides)
+        , button [ onClick ApplyOverrides ] [ text "Apply Overrides" ]
         ]
