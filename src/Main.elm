f8561b82ba717f8680d09b2c1517d897d112a649
Tristan Pendergrass <tristanp@dropbox.com>
2019-11-07 12:09:13 -0800

    Refactor query params logic into its own module

--- src/Main.elm
+++ src/Main.elm
@@ -5,153 +5,13 @@ import Html exposing (Html, button, div, li, option, select, span, text, ul)
 import Html.Attributes exposing (value)
 import Html.Events exposing (onClick, onInput)
 import List
-import Regex
-import String
+import QueryParams
 import Url exposing (Url)
 
 
 main : Program String Model Msg
 main =
     Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
-
-
-colon : Regex.Regex
-colon =
-    Maybe.withDefault Regex.never <|
-        Regex.fromString ":|%3A"
-
-
-queryParamsFromUrl : Url -> List QueryParam
-queryParamsFromUrl url =
-    case url.query of
-        Nothing ->
-            []
-
-        Just query ->
-            query
-                |> String.split "&"
-                |> List.map queryParamFromString
-
-
-{-| Turn a string into a QueryParam.
-
-  - Will make it a StormcrowParam if it fits the format. e.g. "stormcrow\_override=foo:bar"
-  - Will make it an OtherParam if it does not fit. e.g. "shtormcrow\_override=foo:bar", "stormcrow\_override=foobar", "bar=baz"
-
--}
-queryParamFromString : String -> QueryParam
-queryParamFromString param =
-    if String.startsWith "stormcrow_override=" param then
-        case
-            param
-                |> String.dropLeft 19
-                |> Regex.split colon
-        of
-            [] ->
-                OtherParam param
-
-            [ feature, variant ] ->
-                StormcrowParam feature variant
-
-            _ ->
-                OtherParam param
-
-    else
-        OtherParam param
-
-
-stringFromQueryParam : QueryParam -> String
-stringFromQueryParam param =
-    case param of
-        OtherParam value ->
-            value
-
-        StormcrowParam feature variant ->
-            "stormcrow_override=" ++ feature ++ ":" ++ variant
-
-
-{-| Iterate over a list of query params and either modify the params matching the override or
-append a new StormcrowParam at the end if no param matched the override
-
-    e.g.
-
-    applyOverride
-        { feature = Feature "foo", selectedVariant = Variant "bar" }
-        [ OtherParam "other=value", StormcrowParam "foo" "baz" ]
-
-    -- equals [ OtherParam "other=value", StormcrowParam "foo" "bar" ]
-
-    -- OR
-
-    applyOverride
-        { feature = Feature "foo", selectedVariant = Variant "bar" }
-        [ OtherParam "other=value", StormcrowParam "baz" "bing" ]
-
-    -- equals [ OtherParam "other=value", StormcrowParam "baz" "bing", StormcrowParam "foo" "bar" ]
-
--}
-applyOverride : Override -> List QueryParam -> List QueryParam
-applyOverride override queryParams =
-    case override.selectedVariant of
-        EmptySelection ->
-            queryParams
-
-        VariantSelection variant ->
-            let
-                ( overridePresent, newQueryParams ) =
-                    List.foldr
-                        (\queryParam ->
-                            \( found, accQueryParams ) ->
-                                case queryParam of
-                                    OtherParam _ ->
-                                        ( found, queryParam :: accQueryParams )
-
-                                    StormcrowParam feature _ ->
-                                        if feature == override.feature then
-                                            ( True, StormcrowParam feature variant :: accQueryParams )
-
-                                        else
-                                            ( found, queryParam :: accQueryParams )
-                        )
-                        ( False, [] )
-                        queryParams
-            in
-            if overridePresent then
-                newQueryParams
-
-            else
-                List.append queryParams [ StormcrowParam override.feature variant ]
-
-
-queryStringFromQueryParams : List QueryParam -> Maybe String
-queryStringFromQueryParams queryParams =
-    case queryParams of
-        [] ->
-            Nothing
-
-        params ->
-            params
-                |> List.map stringFromQueryParam
-                |> String.join "&"
-                |> Just
-
-
-applyOverridesToUrl : List Override -> Url -> Url
-applyOverridesToUrl overrides oldUrl =
-    let
-        oldParams : List QueryParam
-        oldParams =
-            queryParamsFromUrl oldUrl
-
-        newParams : List QueryParam
-        newParams =
-            List.foldl applyOverride oldParams overrides
-
-        newUrl : Url
-        newUrl =
-            { oldUrl | query = queryStringFromQueryParams newParams }
-    in
-    newUrl
 
 
 
@@ -162,22 +22,8 @@ type alias Id =
     Int
 
 
-type alias Feature =
-    String
-
-
-type alias Variant =
-    String
-
-
-type QueryParam
-    = StormcrowParam Feature Variant
-    | OtherParam String
-
-
-type VariantSelection
-    = EmptySelection
-    | VariantSelection Variant
+type alias VariantSelection =
+    Maybe QueryParams.Variant
 
 
 type alias Override =
@@ -203,7 +49,7 @@ init initialBrowserUrl =
             [ { id = 0
               , feature = "foo"
               , variants = [ "OFF", "ON" ]
-              , selectedVariant = EmptySelection
+              , selectedVariant = Nothing
               }
             ]
       }
@@ -222,6 +68,35 @@ type Msg
     = SetBrowserUrl String
     | HandleSelectedVariantInput Override String
     | ApplyOverrides
+
+
+applyOverridesToUrl : List Override -> Url -> Url
+applyOverridesToUrl overrides oldUrl =
+    let
+        oldParams : List QueryParams.QueryParam
+        oldParams =
+            QueryParams.fromUrl oldUrl
+
+        newParams : List QueryParams.QueryParam
+        newParams =
+            List.foldl
+                (\override ->
+                    \accumulator ->
+                        case override.selectedVariant of
+                            Nothing ->
+                                accumulator
+
+                            Just variant ->
+                                QueryParams.applyOverride override.feature variant accumulator
+                )
+                oldParams
+                overrides
+
+        newUrl : Url
+        newUrl =
+            { oldUrl | query = QueryParams.toString newParams }
+    in
+    newUrl
 
 
 replace : a -> a -> List a -> List a
@@ -249,10 +124,10 @@ update msg model =
                     { override
                         | selectedVariant =
                             if selection == "" then
-                                EmptySelection
+                                Nothing
 
                             else
-                                VariantSelection selection
+                                Just selection
                     }
 
                 newOverrides : List Override
@@ -301,10 +176,10 @@ renderOverride override =
         selectValue : String
         selectValue =
             case override.selectedVariant of
-                EmptySelection ->
+                Nothing ->
                     ""
 
-                VariantSelection value ->
+                Just value ->
                     value
     in
     li []
---
+++ src/QueryParams.elm
@@ -0,0 +1,115 @@
+module QueryParams exposing (Feature, QueryParam(..), Variant, applyOverride, fromUrl, toString)
+
+import Regex
+import Url exposing (Url)
+
+
+type alias Feature =
+    String
+
+
+type alias Variant =
+    String
+
+
+type QueryParam
+    = StormcrowParam Feature Variant
+    | OtherParam String
+
+
+colon : Regex.Regex
+colon =
+    Maybe.withDefault Regex.never <|
+        Regex.fromString ":|%3A"
+
+
+{-| Decode a string as a query param.
+e.g.
+- singleFromString "foo=bar" == OtherParam "foo=bar"
+- singleFromString "stormcrow\_override=foo:bar" == StormcrowParam "foo" "bar"
+-}
+singleFromString : String -> QueryParam
+singleFromString param =
+    if String.startsWith "stormcrow_override=" param then
+        case
+            param
+                |> String.dropLeft 19
+                |> Regex.split colon
+        of
+            [] ->
+                OtherParam param
+
+            [ feature, variant ] ->
+                StormcrowParam feature variant
+
+            _ ->
+                OtherParam param
+
+    else
+        OtherParam param
+
+
+fromUrl : Url -> List QueryParam
+fromUrl url =
+    case url.query of
+        Nothing ->
+            []
+
+        Just query ->
+            query
+                |> String.split "&"
+                |> List.map singleFromString
+
+
+singleToString : QueryParam -> String
+singleToString param =
+    case param of
+        OtherParam value ->
+            value
+
+        StormcrowParam feature variant ->
+            "stormcrow_override=" ++ feature ++ ":" ++ variant
+
+
+{-| Checks a list of query params for a certain feature, and changes its variant if found.
+If not found, constructs and appends a new StormcrowOverride query to the list.
+-}
+applyOverride : Feature -> Variant -> List QueryParam -> List QueryParam
+applyOverride overrideFeature overrideVariant oldQueryParams =
+    let
+        ( overridePresent, newQueryParams ) =
+            List.foldr
+                (\queryParam ->
+                    \( found, accQueryParams ) ->
+                        case queryParam of
+                            OtherParam _ ->
+                                ( found, queryParam :: accQueryParams )
+
+                            StormcrowParam feature _ ->
+                                if feature == overrideFeature then
+                                    ( True, StormcrowParam feature overrideVariant :: accQueryParams )
+
+                                else
+                                    ( found, queryParam :: accQueryParams )
+                )
+                ( False, [] )
+                oldQueryParams
+    in
+    if overridePresent then
+        newQueryParams
+
+    else
+        List.append oldQueryParams [ StormcrowParam overrideFeature overrideVariant ]
+
+
+toString : List QueryParam -> Maybe String
+toString queryParams =
+    case queryParams of
+        [] ->
+            Nothing
+
+        params ->
+            params
+                |> List.map singleToString
+                |> String.join "&"
+                |> Just
