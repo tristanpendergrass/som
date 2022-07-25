2cb3e110d2bb36df91290906a69ca24fc065f836
Tristan Pendergrass <tristanp@dropbox.com>
2022-07-25 12:22:17 -0700

    Add some more files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx,elm}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
